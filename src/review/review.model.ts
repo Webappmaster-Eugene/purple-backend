560f6e02d23c7146a2a8c1dc84beca0529ec442c
Webappmaster-Eugene <johnn.hotmail@mail.ru>
2024-02-09 15:21:32 +0300

    release before tests

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import { prop } from '@typegoose/typegoose';
+import { Types } from 'mongoose';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface ReviewModel extends Base {}
@@ -19,5 +20,5 @@ export class ReviewModel extends TimeStamps {
   rating: number;
 
   @prop()
-  cratedAt: Date;
+  productId: Types.ObjectId;
 }
